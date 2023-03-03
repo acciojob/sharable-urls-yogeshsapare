1e0a8e2362155878f5b32a07bf5afd3eac11f2c5
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-03-03 13:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+function formSubmit(event)
+{
+  event.preventDefault()
+  const name = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+  const url = document.getElementById("url");
+  console.log(url.innerHTML)
+  console.log(year)
+  if(name && year)
+  {
+    url.innerText = "https://localhost:8080/" + `?name=${name}`+`&year=${year}`;
+  }
+  else if(year && !name)
+  {
+    url.innerText = "https://localhost:8080/" + `?year=${year}`;
+  }
+  else if(!year && name)
+  {
+    url.innerText = "https://localhost:8080/" + `?name=${name}`;
+  }
+ 
+}
